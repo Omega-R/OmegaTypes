1d129e29319eb8d63f7a386bb832ac6656fbb3de
maximlozhechnikov <98010383+maximlozhechnikov@users.noreply.github.com>
2022-09-19 11:26:48 +0300

    add plus and from stub to omegatype

--- stub/src/main/java/com/omega_r/libs/omegatypes/Text.kt
+++ stub/src/main/java/com/omega_r/libs/omegatypes/Text.kt
@@ -5,14 +5,25 @@ import java.lang.RuntimeException
 
 interface Text : Serializable, Textable {
 
+    companion object {
+
+        @JvmStatic
+        @JvmOverloads
+        fun from(string: String, textStyle: TextStyle? = null): Text = throw Exception()
+
+    }
+
     interface StringHolder {
 
         fun getStringText(): String?
 
     }
 
+    operator fun plus(text: Text): Text = throw Exception()
+
 }
 
 abstract class TextStyle : Serializable
 
 fun String.toText(textStyle: TextStyle? = null): Text = throw RuntimeException()
+
